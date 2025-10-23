2f8c908302b4d270c3b34d357534c8eb341ccd25
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-10-23 20:57:22 +0200

    Add deleteCategory functionality to CategoryService and Controller

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -24,4 +24,15 @@ public class CategoryServiceImpl implements CategoryService {
         category.setCategoryId(nextId++);
         categories.add(category);
     }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+
+        Category category = categories.stream()
+                .filter( c -> c.getCategoryId().equals(categoryId))
+                .findFirst()
+                .get();
+        categories.remove(category);
+        return "category deleted with id: " + categoryId;
+    }
 }
