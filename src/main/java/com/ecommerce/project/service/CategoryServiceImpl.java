5b697455ce297ce7f64a6282b03c94cb572f91d1
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-10-28 07:49:46 +0100

    create Repository with JPA-Interface and refactor the calls in the Service to call on the CategoryRepository

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,36 +1,37 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.nio.file.ReadOnlyFileSystemException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-    private Long nextId = 1L;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
-    private List<Category> categories =
-            new ArrayList<>();
+    private Long nextId = 1L;
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll() ;
     }
 
     @Override
     public void createCategory(Category category) {
         category.setCategoryId(nextId++);
-        categories.add(category);
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
 
+        List<Category> categories = categoryRepository.findAll();
         Category category = categories.stream()
                 .filter( c -> c.getCategoryId().equals(categoryId))
                 .findFirst()
@@ -39,18 +40,20 @@ public class CategoryServiceImpl implements CategoryService {
         if (category == null)
             return "Category not found";
 
-        categories.remove(category);
+        categoryRepository.delete(category);
         return "category deleted with id: " + categoryId;
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
+        List<Category> categories = categoryRepository.findAll();
+
         Category updateCategory = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
 
         updateCategory.setCategoryName(category.getCategoryName());
-        return updateCategory;
+        return categoryRepository.save(updateCategory);
     }
 }
