3aba0fb1cc578db4a6628833fe109cb9e578ec40
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-10-29 15:48:25 +0100

    refactor: return BadRequest as HTTP.Status in CustomErrorMessage

--- src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.ecommerce.project.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,13 +14,13 @@ import java.util.Map;
 public class MyGlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> myMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    public ResponseEntity<Map<String, String>> myMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         Map<String, String> response = new HashMap<>();
         exception.getBindingResult().getAllErrors().forEach( error -> {
             String fieldName = ((FieldError) error).getField();
             String message = error.getDefaultMessage();
             response.put(fieldName, message);
         });
-        return response;
+        return new ResponseEntity<Map<String, String>>(response, HttpStatus.BAD_REQUEST);
     }
 }
