dc76deca1e486086ecf7a9dee4b2999b0ef5f5be
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-10-30 20:11:59 +0100

    refactor: Implementing DTOs For DeleteCategory

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -48,18 +47,16 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public String deleteCategory(Long categoryId) {
+    public CategoryDTO deleteCategory(Long categoryId) {
         Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException("Category","categoryId",categoryId));
         categoryRepository.delete(category);
-        return "Category with" + category.getCategoryName() + " deleted";
+        return modelMapper.map(category,CategoryDTO.class);
     }
 
     @Override
     public CategoryDTO updateCategory(CategoryDTO categoryDTO, Long categoryId) {
-
         Category savedCategory = categoryRepository.findById(categoryId)
                 .orElseThrow( () -> new ResourceNotFoundException("Category","categoryId",categoryId));
-
         Category updateCategory = modelMapper.map(categoryDTO, Category.class);
         updateCategory.setCategoryId(savedCategory.getCategoryId());
         Category savedCategoryEntity = categoryRepository.save(updateCategory);
