de46f026cc4cf58f5c0d560d0d23a671c85d8f41
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-11-26 21:07:44 +0100

    add missed annotations and define path api/auth

--- src/main/java/com/ecommerce/project/controller/AuthController.java
+++ src/main/java/com/ecommerce/project/controller/AuthController.java
@@ -25,10 +25,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
 
-@Controller
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
