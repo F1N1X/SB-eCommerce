d5d11f34ae4b9a59f685c950e5c92fea9d15af47
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-12-02 11:39:27 +0100

    AuthUtils Class with getUser/name/id from auth from context data

--- src/main/java/com/ecommerce/project/service/CartServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CartServiceImpl.java
@@ -10,6 +10,7 @@ import com.ecommerce.project.payload.ProductDTO;
 import com.ecommerce.project.repositories.CartItemRepository;
 import com.ecommerce.project.repositories.CartRepository;
 import com.ecommerce.project.repositories.ProductRepository;
+import com.ecommerce.project.util.AuthUtil;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -33,7 +34,7 @@ public class CartServiceImpl implements CartService{
     ModelMapper modelmapper;
 
     @Autowired
-    AuthUltil authUtil;
+    AuthUtil authUtil;
 
     @Override
     public CartDTO addProductToCart(Long productId, Integer quantity) {
