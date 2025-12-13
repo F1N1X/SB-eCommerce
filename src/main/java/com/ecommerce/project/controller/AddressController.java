54f8958d63a73dc561b8e2ee43ff82b133be1517
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-12-13 10:30:44 +0100

    Creating Addresses fixing api for null/validation of creating address

--- src/main/java/com/ecommerce/project/controller/AddressController.java
+++ src/main/java/com/ecommerce/project/controller/AddressController.java
@@ -5,17 +5,19 @@ import com.ecommerce.project.model.User;
 import com.ecommerce.project.payload.AddressDTO;
 import com.ecommerce.project.service.AddressService;
 import com.ecommerce.project.util.AuthUtil;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping("/api")
-public class AdressController {
+public class AddressController {
 
     @Autowired
     private AddressService addressService;
@@ -24,7 +26,8 @@ public class AdressController {
     AuthUtil authUtil;
 
     @PostMapping("/address")
-    public ResponseEntity<AddressDTO> createAddress(AddressDTO addressDTO) {
+    public ResponseEntity<AddressDTO> createAddress(
+            @Valid @RequestBody AddressDTO addressDTO) {
         User user = authUtil.loggedInUser();
         AddressDTO savedAddressDTO = addressService.createAddress(addressDTO, user);
         return new ResponseEntity<>(savedAddressDTO, HttpStatus.CREATED);
