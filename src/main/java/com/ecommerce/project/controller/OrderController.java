7fb5ff83b64414b766aa354e6944c1a037b87b3a
F1NiX <73650428+F1N1X@users.noreply.github.com>
2025-12-27 16:00:20 +0100

    create swagger api-doku for Order-Controller

--- src/main/java/com/ecommerce/project/controller/OrderController.java
+++ src/main/java/com/ecommerce/project/controller/OrderController.java
@@ -4,11 +4,20 @@ import com.ecommerce.project.payload.OrderDTO;
 import com.ecommerce.project.payload.OrderRequestDTO;
 import com.ecommerce.project.service.OrderService;
 import com.ecommerce.project.util.AuthUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+@Tag(
+        name = "Orders",
+        description = "Endpoints for placing and managing customer orders"
+)
+@SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequestMapping("api")
 public class OrderController {
@@ -20,6 +29,10 @@ public class OrderController {
     private AuthUtil authUtil;
 
 
+    @Operation(
+            summary = "Place an order",
+            description = "Places a new order for the logged-in user using the selected payment method"
+    )
     @PostMapping("/order/users/payments/{paymentMethod}")
     public ResponseEntity<OrderDTO> orderProducts(
             @PathVariable String paymentMethod,
